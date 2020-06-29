e605f52e743d103b2f6d4d48cb8135d8975f57ac
contradictory <lsy81056@gmail.com>
2020-06-29 11:57:12 -0400

    third commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
         String[] names = new String[]{"Abby Truong", "Ahmed Mujtaba", "Azeez Temitope Olanrewaju", "Diming Chu", "Dongshuo Li", "Honorinicy",
                                      "Jason", "Jason", "Kelly Liu", "Kinjan", "Kun Li", "Muhammed Umar", "Oaz", "PQ", "Prithviatani", "Richard",
                                      "Sahil Slngh Sandhu", "Samuel Yang", "Scott(Shi-Kai) Jhou", "Shahbaz Mansahia", "Shahriar Kabir", "Shakur",
-                                     "Shrina Patel", "Siyu Qiu", "Siyu Lei", "Sumid", "Sumid", "Trivediakshay", "Trivediakshay", "Zhaosaitong"};
+                                     "Shrina Patel", "Siyu Qiu", "Siyu Lei", "Sumid", "Tao", "Trivediakshay", "Yan Huang", "Zhaosaitong"};
 
         double ran = 0 + Math.random() * (29 + 1);
         String res = names[(int)ran];
