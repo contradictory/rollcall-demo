992ec26e1bf551a5d02b5a446fb848d1d6207036
contradictory <lsy81056@gmail.com>
2020-06-30 09:51:22 -0400

    4th commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,23 @@
 package main;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("Hello world");
-        String[] names = new String[]{"Abby Truong", "Ahmed Mujtaba", "Azeez Temitope Olanrewaju", "Diming Chu", "Dongshuo Li", "Honorinicy",
+        String[] input = new String[]{"Abby Truong", "Ahmed Mujtaba", "Azeez Temitope Olanrewaju", "Diming Chu", "Dongshuo Li", "Honorinicy",
                                      "Jason", "Jason", "Kelly Liu", "Kinjan", "Kun Li", "Muhammed Umar", "Oaz", "PQ", "Prithviatani", "Richard",
                                      "Sahil Slngh Sandhu", "Samuel Yang", "Scott(Shi-Kai) Jhou", "Shahbaz Mansahia", "Shahriar Kabir", "Shakur",
                                      "Shrina Patel", "Siyu Qiu", "Siyu Lei", "Sumid", "Tao", "Trivediakshay", "Yan Huang", "Zhaosaitong"};
+        ArrayList<String> names = new ArrayList<>();
+//        names.addAll(input);
+        for (String i : input) {
+            names.add(i);
+        }
 
         double ran = 0 + Math.random() * (29 + 1);
-        String res = names[(int)ran];
+        String res = names.get((int)ran);
         System.out.println(res);
         JOptionPane.showConfirmDialog(null, res, null, JOptionPane.YES_NO_OPTION);
     }
